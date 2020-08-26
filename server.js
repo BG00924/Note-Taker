9addd8bb134db74654a0c7c8aefb7d18c53a119e
BG00924 <BG00924@users.noreply.github.com>
2020-08-26 11:06:35 -0500

    stuck on post

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const apiRoutes = require('')
-const htmlRoutes = require('')
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
 const PORT = process.env.PORT || 3001
 const app = express()
 app.use('/api', apiRoutes)
