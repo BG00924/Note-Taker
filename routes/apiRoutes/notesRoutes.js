213d6792d8795230589a43c2572e87c26a73ed6e
BG00924 <BG00924@users.noreply.github.com>
2020-08-26 13:24:35 -0500

    added id feature to notes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -13,7 +13,7 @@ router.get('/notes', (req, res) => {
     }
 })
 router.post('/notes', (req, res) => {
-    // req.body.id = db.length.toString()
+    req.body.id = db.length.toString()
     // if (!validateNote(req.body)) {
     //     res.status(400).send('The note is not properly formatted.')
     // } else {
